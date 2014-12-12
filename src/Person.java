7a33cc0762775936e3ad880cac579f898c8c4355
tanvinayak6 <tncces1@gmail.com>
2014-12-12 07:39:50 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,41 @@
 // Super Class to represent a person
+// Super Class to represent a person
+public class Person 
+{
+  private String first;
+  private String last;
+  private int age;
+  public Person()
+  {
+    first="";
+    last="";
+    age=0;
+  }
+  public Person(String f, String l, int age)
+  {
+    first=f;
+    last=l;
+    this.age=age;
+  }
+  public String getFirstName()
+  {
+    return first;
+  }
+  public String getLastName()
+  {
+    return last;
+  }
+  public int getAge()
+  {
+    return age;
+  }
+  public String toString()
+  {
+    return last+", "+first;
+  }
+  public void recordBirthday()
+  {
+    age++;
+  }
+}
+    
