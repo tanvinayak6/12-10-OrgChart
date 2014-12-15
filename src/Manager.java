47ee3de99c9f0f1cacb02dbfb9f8ff151f56e231
tanvinayak6 <tncces1@gmail.com>
2014-12-15 16:59:42 -0700

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,19 +1,19 @@
-// class to represent a manager - inherits Emplyee
-import java.util.ArrayList;
+==import java.util.ArrayList;
 // class to represent a manager - inherits Emplyee
 public class Manager extends Employee
 {
   private ArrayList<Employee> directReports;
   private String department;
-  public Manager(String first, String last, int age, double wage, int hours, String job, ArrayList<Employee> list, String dept )
+  public Manager(String first, String last, int age, double wage, int hours, String job, Manager m, ArrayList<Employee> list, String dept )
   {
-    super(first, last, age, wage, hours, job);
+    super(first, last, age, wage, hours, job,m);
     for(int i=0; i<list.size(); i++)
     {
       directReports.set(i, (list.get(i)));
       department= dept;
    }
   }
+
   public String getDepartment()
   {
     return department;
@@ -24,7 +24,7 @@ public class Manager extends Employee
   }
   public String toString()
   {
-    return "Manager: "+super.toString();
+    return "Manager: "+super.toString()+ "\n employees: "+directReports;
   }
   public void addDirectReport(Employee emp1)
   {
@@ -37,6 +37,17 @@ public class Manager extends Employee
   public void setDepartment( String dept)
   {
     department=dept;
+  }
+ public boolean equals(Object o)
+  {
+    
+    if(o instanceof Manager){
+      Manager other = (Manager) o;
+      return getFirstName()== other.getFirstName() && getLastName()==other.getLastName() && getAge()==other.getAge() &&getWage()==other.getWage() && getHours() == other.getHours() && getJobTitle()==other.getJobTitle()&& getManager()==other.getManager()&& department==other.getDepartment();
+    }
+    else{
+      return false;
+    }
   }
 }
     
