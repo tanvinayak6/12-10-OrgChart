ac1123521463883ebfaa6ec457454c72e148ad31
tanvinayak6 <tncces1@gmail.com>
2014-12-15 17:00:12 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,41 +1,54 @@
-// Super Class to represent a person
-// Super Class to represent a person
-public class Person 
+import java.util.ArrayList;
+// class to represent a manager - inherits Emplyee
+public class Manager extends Employee
 {
-  private String first;
-  private String last;
-  private int age;
-  public Person()
+  private ArrayList<Employee> directReports;
+  private String department;
+  public Manager(String first, String last, int age, double wage, int hours, String job, Manager m, ArrayList<Employee> list, String dept )
   {
-    first="";
-    last="";
-    age=0;
+    super(first, last, age, wage, hours, job,m);
+    for(int i=0; i<list.size(); i++)
+    {
+      directReports.set(i, (list.get(i)));
+      department= dept;
+   }
   }
-  public Person(String f, String l, int age)
+
+  public String getDepartment()
   {
-    first=f;
-    last=l;
-    this.age=age;
+    return department;
   }
-  public String getFirstName()
+  public ArrayList<Employee> getDirectReports()
   {
-    return first;
-  }
-  public String getLastName()
-  {
-    return last;
-  }
-  public int getAge()
-  {
-    return age;
+    return directReports;
   }
   public String toString()
   {
-    return last+", "+first;
+    return "Manager: "+super.toString()+ "\n employees: "+directReports;
   }
-  public void recordBirthday()
+  public void addDirectReport(Employee emp1)
   {
-    age++;
+    directReports.add(emp1);
+  }
+  public void removeDirectReport(Employee emp1)
+  {
+    directReports.remove(emp1);
+  }
+  public void setDepartment( String dept)
+  {
+    department=dept;
+  }
+ public boolean equals(Object o)
+  {
+    
+    if(o instanceof Manager){
+      Manager other = (Manager) o;
+      return getFirstName()== other.getFirstName() && getLastName()==other.getLastName() && getAge()==other.getAge() &&getWage()==other.getWage() && getHours() == other.getHours() && getJobTitle()==other.getJobTitle()&& getManager()==other.getManager()&& department==other.getDepartment();
+    }
+    else{
+      return false;
+    }
   }
 }
     
+  
